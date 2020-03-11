39d7fa1244e924fda1864bd8483e1790fc3a487b
Pierre <u18113941@tuks.co.za>
2020-03-11 18:23:44 +0200

    Simple interface

--- app/src/main/java/com/omega/mouthpiece/MainActivity.java
+++ app/src/main/java/com/omega/mouthpiece/MainActivity.java
@@ -2,13 +2,26 @@ package com.omega.mouthpiece;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    AnimationDrawable mouthAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView mouthImage = findViewById(R.id.img_mouth);
+        mouthImage.setBackgroundResource(R.drawable.mouth_animation);
+        mouthAnimation = (AnimationDrawable) mouthImage.getBackground();
+
+        mouthImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mouthAnimation.start();
+            }
+        });
     }
 }
