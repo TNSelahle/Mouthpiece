c94231a910f22655271b9c6ef6c900c68d89f3f4
JohanWol <u17043540@tuks.co.za>
2020-03-12 22:33:21 +0200

    Fixed buttons that weren't both being displayed, also added Jaco's email code app should run properly now

--- Notifications test app/Notifications/app/src/main/java/com/example/notifications/EmailNotification.java
+++ Notifications test app/Notifications/app/src/main/java/com/example/notifications/EmailNotification.java
@@ -1,83 +1,5 @@
 package com.example.notifications;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import com.sun.mail.smtp.SMTPTransport;
-import java.util.Properties;
-import javax.mail.*;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-
-public class EmailNotification {
-
-    static final String SMTP_HOST ="smtp.gmail.com";
-    private static final String SMTP_AUTH_USER = "cosomegatst@gmail.com";
-    private static final String SMTP_AUTH_PWD  = "Omega321##";
-
-    public static void createEmail(JSONObject json)
-    {
-        Properties properties = System.getProperties();
-        properties.put("mail.smtp.host", SMTP_HOST);
-        properties.put("mail.smtp.port","465"); //465
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.socketFactory.port", "465");
-        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-
-//    Session session = Session.getInstance(properties, null);
-//    session.setDebug(true);
-        Authenticator auth = new SMTPAuthenticator();
-        Session session = Session.getDefaultInstance(properties,auth);
-
-        try{
-            MimeMessage message = new MimeMessage(session);
-
-            message.setFrom(new InternetAddress(SMTP_AUTH_USER));
-            message.addRecipient(Message.RecipientType.TO ,new InternetAddress(json.getString("Email")));
-            message.setSubject("Test the email");
-            message.setText("This is the body of the email"+json.getString("Msg"));
-
-            System.out.println("Sending the email");
-
-            Transport t = session.getTransport();
-            t.connect();
-            t.sendMessage(message,message.getRecipients(Message.RecipientType.TO));
-            t.close();
-//           SMTPTransport t = (SMTPTransport)session.getTransport("smtp");
-//           t.connect("smtp.gmail.com",465,"cosomegatest@gmail.com", "Omega312##");
-//           t.sendMessage(message, message.getAllRecipients());
-            System.out.println("The email sent successfully");
-
-
-        }
-        catch (AddressException e)
-        {
-            System.out.println(e);
-        }
-        catch (MessagingException e)
-        {
-            System.out.println(e);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static class SMTPAuthenticator extends javax.mail.Authenticator {
-        public PasswordAuthentication getPasswordAuthentication() {
-            String username = SMTP_AUTH_USER;
-            String password = SMTP_AUTH_PWD;
-            return new PasswordAuthentication(username, password);
-        }
-    }
-}
-
-/*
-
-
-package emailtest;
-
         import com.sun.mail.smtp.SMTPTransport;
         import java.util.Properties;
         import javax.mail.*;
@@ -87,11 +9,11 @@ package emailtest;
         import javax.mail.Authenticator;
         import javax.mail.PasswordAuthentication;
 
-public class EmailTest {
+public class EmailNotification {
 
 
     private static final String SMTP_HOST_NAME = "smtp.gmail.com";
-    private static final String SMTP_AUTH_USER = "cosomegatst@gmail.com";
+    private static final String SMTP_AUTH_USER = "cosomegatest@gmail.com";
     private static final String SMTP_AUTH_PWD  = "Omega321##";
     public static void main(String[] args)  {
 
@@ -155,38 +77,4 @@ public class EmailTest {
         }
     }
 }
- */
 
-//the old code is at the bottom here
-/*
-        try{
-            String from = "cosomegatest@gmail.com";
-            String emailAddress = json.getString("Email");
-            String userName = json.getString("User");
-            String emailType = json.getString("Msg");//verify
-
-            String host ="smtp.gmail.com";
-
-            Properties prop = System.getProperties();
-            prop.setProperty("mail.smtp.host","localhost");
-            Session session=Session.getDefaultInstance(prop);
-
-            MimeMessage message=new MimeMessage(session);
-
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(MimeMessage.RecipientType.TO ,new InternetAddress(emailAddress));
-            message.setSubject("This is a test Email");
-
-//            message.setHeader("Hi Cos 301 Omega Notifications");
-            message.setText("THe followiing message was sent to user: "+userName+"\nwitht the message: "+emailType);
-
-            Transport.send(message);
-            System.out.println("The message was sent corretly");
-        }
-        catch(JSONException | AddressException e)
-        {
-            System.out.println("error creating email with json data: "+e);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
- */
--- Notifications test app/Notifications/app/src/main/java/com/example/notifications/MainActivity.java
+++ Notifications test app/Notifications/app/src/main/java/com/example/notifications/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v)
            {
                JSONObject json = getJSON();
-               EmailNotification.createEmail(json);
            }
        });
 
