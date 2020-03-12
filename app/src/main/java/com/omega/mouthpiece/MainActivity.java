3ded98112f0f290f8e8c9746655ac442ff157339
Pierre <u18113941@tuks.co.za>
2020-03-12 16:10:28 +0200

    Handler function + runnable var + basic open and close + amplitude to decibels conversion

--- app/src/main/java/com/omega/mouthpiece/MainActivity.java
+++ app/src/main/java/com/omega/mouthpiece/MainActivity.java
@@ -1,5 +1,6 @@
 package com.omega.mouthpiece;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -8,24 +9,44 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.IOException;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
     AnimationDrawable MouthAnimation;
+    //------------------------RECORDING VAR----------------------------------
     private MediaRecorder recorder = null;
     private static final String LOG_TAG = "AudioRecordTest";
     private static String fileName = null;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
     private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    //-----------------------------------------------------------------------
 
+    //------------------------HANDLER VAR--------------------------------------
+    Handler h2 = new Handler();
+    //-----------------------------------------------------------------------
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
 
     private void startRecording() {
         recorder = new MediaRecorder();
@@ -49,10 +70,48 @@ public class MainActivity extends AppCompatActivity {
         recorder.release();
         recorder = null;
     }
+
+    //----------------------------------MEASURE AMP------------------------------
+    public double getAmplitude() {
+        if (recorder != null)
+            return  (recorder.getMaxAmplitude());
+        else
+            return 0;
+
+    }
+    //-----------------------------------------------------------------------------
+    boolean open = true;
+    //-----------------------TIMER FUNCTIONS---------------------------------------
+    Runnable measure = new Runnable() {
+
+        @Override
+        public void run() {
+            ImageView mouthImage = findViewById(R.id.img_mouth);
+            double amp = getAmplitude();
+            double db = 20 * Math.log10(amp / 0.447);
+            if(db >= 70) {
+                mouthImage.setBackgroundResource(R.drawable.open_mouth);
+            }
+            else {
+                mouthImage.setBackgroundResource(R.drawable.close_mouth);
+            }
+            MouthAnimation = (AnimationDrawable) mouthImage.getBackground();
+            MouthAnimation.start();
+
+            h2.postDelayed(this, 0);
+        }
+    };
+    //---------------------------------------------------------------------------------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //---------------------------ANIMATION INIT------------------------------
+        ImageView mouthImage = findViewById(R.id.img_mouth);
+        mouthImage.setBackgroundResource(R.drawable.open_mouth);
+        MouthAnimation = (AnimationDrawable) mouthImage.getBackground();
+
         //---------------------------RECORDING SYSTEM-----------------------------
         // Record to the external cache directory for visibility
         fileName = getExternalCacheDir().getAbsolutePath();
@@ -63,22 +122,24 @@ public class MainActivity extends AppCompatActivity {
         Button volume_based_btn = findViewById(R.id.btn_record);
         //Click on Volume button to start recording
         volume_based_btn.setOnClickListener(new View.OnClickListener() {
+            boolean mStartRecording = true;
             public void onClick(View view) {
-                startRecording();
+
+                if (mStartRecording) {//click to record
+                    startRecording();
+                    h2.postDelayed(measure,0);
+                } else {//click again to stop recording
+                    stopRecording();
+                    h2.removeCallbacks(measure);
+                }
+                mStartRecording = !mStartRecording;
             }
         });
-        //-------------------------MEASURE AUDIO INTENSITY-------------------------------
-        //TODO
-        //---------------------------ANIMATION------------------------------
-        ImageView mouthImage = findViewById(R.id.img_mouth);
-        mouthImage.setBackgroundResource(R.drawable.open_mouth);
-        MouthAnimation = (AnimationDrawable) mouthImage.getBackground();
 
         //-------------------------TESTING--------------------------------
         //Click On image to check animation
         mouthImage.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                MouthAnimation.start();
             }
         });
 
