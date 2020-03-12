6c60b41d4c0b15be4c8d3c409bcd60d762364e22
Ronald <u15226532@tuks.co.za>
2020-03-12 13:08:31 +0200

    Updated project

---
+++ app/src/main/java/com/omega/mouthpiece/mouth_Display.java
@@ -0,0 +1,14 @@
+package com.omega.mouthpiece;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mouth_Display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mouth__display);
+    }
+}
