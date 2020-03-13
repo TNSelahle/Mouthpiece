32445fc5ddb81c84c8a9d39ad1ca86c53fea9e0a
@jay-lutz <lutz.jason614@gmail.com>
2020-03-13 13:07:41 +0200

    Update handler

--- NNModule/handler.py
+++ NNModule/handler.py
@@ -1,49 +1,48 @@
 import random as rn
 
+
 class NN:
     def __init__(self):
         rn.seed(12241)
 
     def classifySegment(self):
-        return rn.randint(0,12)
+        return rn.randint(0, 12)
+
+
+class Phoneme:
+    def __init__(self):
+        self.phoneme = 0
+
 
 class Handler:
-    
-    #"""Version 1"""
+
+    # """Version 1"""
     def __init__(self):
         self.phoneticQueue = []
+        self.inputQueue = []
         self.network = NN()
 
     def classifySegment_V1(self):
-        return self.network.classifySegment()#between 1 and 12
+        return self.network.classifySegment()  # between 1 and 12
 
     def getPhonetic_V1(self):
-         #add buffer to queue
-        phonetic = self.classifySegment_V1()
-        self.addSegment_V1(phonetic)
+        phonetic = Phoneme()
+        phonetic.phoneme = self.classifySegment_V1()
+        self.phoneticQueue.append(phonetic)
         return
 
-    def addSegment_V1(self, Segment):
-        self.phoneticQueue.append(Segment)
+    def addSegment_V1(self, segment):
+        self.phoneticQueue.append(segment)
         return
 
     def trainAI_V1(self):
         return
 
-    #"""Version 2"""
-    #def GetPhonetic_V2(Segment):
-     #   ClassifySegment_V2(Segment)
-
-
-    #def ClassifySegment_V2(Segment):
-
-    #def TrainAI_V2(VoiceSegement):
 
 hand = Handler()
 
-for x in range(0,10):
+
+for x in range(0, 10):
     hand.getPhonetic_V1()
 
 print(hand.phoneticQueue)
-
- 
