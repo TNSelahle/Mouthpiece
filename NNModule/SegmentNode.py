7436cbc94d2e00ddb07fbef240e88b1a233693fe
Gian Uys <gianuys@gmail.com>
2020-03-13 14:41:42 +0200

    Unit Testing
    Added Initial Code

--- NNModule/SegmentNode.py
+++ NNModule/SegmentNode.py
@@ -1,5 +1,8 @@
+print('imported segmentnode')
+
 class SegmentNode:
-    def __init__(self, amplitude, time, label):
-        self.amplitude = amplitude
-        self.time = time
-        self.label = label
+    def __init__(self, fv):
+        self.frequencyVector = fv
+    
+    def getsomething(self):
+        return 0
---
+++ NNModule/output.py
@@ -0,0 +1,6 @@
+import random
+
+amps = []
+for n in range(0, 500):
+    amps.append(random.uniform(-1, 1))
+return amps
