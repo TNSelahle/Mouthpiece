60e949847fa91449851f5632ce8a0b764fbe233f
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-03-26 20:00:22 +0200

    unit test draft
    unit test draft

--- app/src/test/java/com/omega/mouthpiece/ExampleUnitTest.java
+++ app/src/test/java/com/omega/mouthpiece/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.omega.mouthpiece;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +12,48 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private MainActivity mainActivity;
+    @Before
+    public void setUp() {
+        mainActivity = new MainActivity();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Done with testing");
+    }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void test() {
+        testGettingFormants();
+        testAmplitude();
+    }
+    public void testAmplitude() {
+        double amplitude = mainActivity.getAmplitude();
+        if(amplitude>0)
+        {
+            System.out.println("recording");
+        }
+        else
+        {
+            System.out.println("Not recording");
+        }
+        // assertEquals("Not recording", 0, amplitude);
+    }
+
+    public void testGettingFormants() {
+        int formant = mainActivity.getFormant();
+        if(formant == 0)
+        {
+            System.out.println("close mouth");
+        }
+        else if(formant > 0 && formant < 13)
+        {
+            System.out.println("other formant");
+        }
+        else
+        {
+            System.out.println("error in formant");
+        }
     }
 }
