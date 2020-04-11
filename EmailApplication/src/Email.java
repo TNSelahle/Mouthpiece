bd1fb01ccc627037697d176df46e1ec38fef515d
Jaco <jaco.fab4@gmail.com>
2020-04-11 18:17:52 +0200

    Email Application Source Files
    The application interface that the server group will use to send emails to the users.. added documentstion  included in the Email Application file

---
+++ EmailApplication/src/Email.java
@@ -0,0 +1,88 @@
+/*
+    Authors: 
+        u18140565 - Jacobus Janse van Rensburg
+
+*/
+
+import org.json.*;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+
+public class Email {
+    private static final String SMTP_HOST_NAME = "smtp.gmail.com";
+    private static final String SMTP_AUTH_USER="cosomegatest@gmail.com";
+    private static final String SMTP_AUTH_PASSWORD="Omega312##";   
+    
+    public static void email(JSONObject json)
+    {
+        
+        System.out.println("Preparing the data to be email");
+            //determine if the email is being sent to multiple or a single person
+
+            //determine who the email is being sent to 
+
+            //set the properties 
+        Properties prop = getProperties();
+
+        Authenticator auth = new SMTPAuthenticator();
+        Session session = Session.getDefaultInstance(prop,auth);
+
+            //detrmine the type of message to email
+
+            //create the message to send depending on the json data
+        try{
+            
+            MimeMessage message = Message.createMessage(json,session);
+
+           //send  message that we will send a email
+           System.out.println("Sending the emaill");
+           Transport t = session.getTransport();
+           t.connect();
+           t.sendMessage(message,message.getRecipients(javax.mail.Message.RecipientType.TO));
+           t.close();
+           
+           System.out.println("Email sent");
+
+        }catch (AddressException e)
+        {
+            System.out.println(e);
+        }
+        catch (MessagingException e)
+        {
+            System.out.println(e);
+        }
+    }
+    
+    
+    public static Properties getProperties()
+    {
+        Properties prop = System.getProperties();
+        prop.put("mail.smtp.host", SMTP_HOST_NAME);
+        prop.put("mail.smtp.port","465"); //465
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        return prop;
+    }
+    
+    
+    public static String getSMTPHostName()
+    {
+        return SMTP_HOST_NAME;
+    }
+    public static String getSMTPAuthUser()
+    {
+        return SMTP_AUTH_USER;
+    }
+    public static String getSMTPAuthPassword()
+    {
+        return SMTP_AUTH_PASSWORD;
+    }
+}
+
---
+++ EmailApplication/src/main.java
@@ -0,0 +1,22 @@
+
+
+import org.json.JSONObject;
+
+public class main {
+    
+    public static void main (String[]args)
+    {
+        JSONObject json = new JSONObject();
+        
+        json.put("UserEmail","");
+        json.put("UserName","squidBro");
+        json.put("MsgType", "validate");
+        json.put("Subject", "My 301 email");
+        json.put("Message","Hello there, General Grievis");
+        
+        
+        Email.email(json);
+        
+    }
+    
+}
