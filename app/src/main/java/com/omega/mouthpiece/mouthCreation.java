109ee0dc9b9ad158e5927a503c57ab895f21ead9
Ronald Looi <u15226532@tuks.co.za>
2020-04-11 19:11:02 +0200

    Base design for mouth creation upload image - temporary

--- app/src/main/java/com/omega/mouthpiece/mouthCreation.java
+++ app/src/main/java/com/omega/mouthpiece/mouthCreation.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.Manifest;
+import android.view.View;
+import android.widget.Button;
 
 public class mouthCreation extends AppCompatActivity {
+
+    private Button btnStartUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mouth_creation);
-    }
 
-    public void onClickHandler(){
-        Intent intent = new Intent(mouthCreation.this, mouthCreation.class);
+    }
+    public void clickHandler(View view){
+        Intent intent = new Intent(mouthCreation.this, mouthCreation_ImageUpload.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/omega/mouthpiece/mouthCreation_ImageUpload.java
+++ app/src/main/java/com/omega/mouthpiece/mouthCreation_ImageUpload.java
@@ -9,7 +9,8 @@ public class mouthCreation_ImageUpload extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mouth_creation_image_upload);
+        setContentView(R.layout.mouth_creation_image_upload);
 
     }
+
 }
