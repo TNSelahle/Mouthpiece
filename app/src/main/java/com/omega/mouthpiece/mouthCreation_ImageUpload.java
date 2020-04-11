a8cef30aa53af3641faf09b166462b7cb1953c49
Ronald Looi <u15226532@tuks.co.za>
2020-04-11 11:22:47 +0200

    Base design for mouth creation upload image - temporary

---
+++ app/src/main/java/com/omega/mouthpiece/mouthCreation_ImageUpload.java
@@ -0,0 +1,15 @@
+package com.omega.mouthpiece;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class mouthCreation_ImageUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mouth_creation_image_upload);
+
+    }
+}
