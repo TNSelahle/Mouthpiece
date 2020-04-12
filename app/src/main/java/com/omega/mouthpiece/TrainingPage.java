75796b54862c8f79a81b098cc636dadaca5396d4
ShaunVermeulen <u16078625@tuks.co.za>
2020-04-12 16:36:24 +0200

    Fixed Voice recording and is now working -Changes made to Training page class

--- app/src/main/java/com/omega/mouthpiece/TrainingPage.java
+++ app/src/main/java/com/omega/mouthpiece/TrainingPage.java
@@ -1,42 +1,37 @@
 package com.omega.mouthpiece;
 
-        import androidx.annotation.NonNull;
-        import androidx.appcompat.app.AppCompatActivity;
-        import androidx.core.app.ActivityCompat;
-
-
-        import android.Manifest;
-        import android.graphics.Color;
-        import android.util.Log;
-        import android.content.Intent;
-        import android.content.pm.PackageManager;
-        import android.media.MediaRecorder;
-        import android.media.MediaPlayer;
-        import android.os.Bundle;
-        import android.os.Environment;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
-        import android.widget.Toast;
-        import java.io.IOException;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.graphics.Color;
+import android.util.Log;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
 
 public class TrainingPage extends AppCompatActivity {
-    private Button EnDisTrain;
-    private Button DisableTrainer;
-    private static String outputF = null;
-    private MediaRecorder trainerAudio;
-    /*
+    //features for audio recording
+    private MediaRecorder trainerAudio = null;
     private static final String LOG_TAG = "AudioRecTest";
+    private static String outputF = null;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
     private String [] permissions = {Manifest.permission.RECORD_AUDIO};
-    //-----------------------------------------------------------------------
 
-    //------------------------HANDLER VAR--------------------------------------
-   // Handler h2 = new Handler();
-    //-----------------------------------------------------------------------
+
+    private Button EnDisTrain;
+    private Button DisableTrainer;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -49,32 +44,51 @@ public class TrainingPage extends AppCompatActivity {
         if (!permissionToRecordAccepted ) finish();
 
     }
-*/
+
+    private void startRecording(){
+
+        trainerAudio = new MediaRecorder();
+        trainerAudio.setAudioSource(MediaRecorder.AudioSource.MIC);
+        trainerAudio.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        trainerAudio.setOutputFile(outputF);
+        trainerAudio.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            trainerAudio.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        trainerAudio.start();
+    }
+    private void stopRecoring(){
+        trainerAudio.stop();
+        trainerAudio.release();
+        trainerAudio = null;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.training_page);
 
+        //Enable the voice training button when first opened
         EnDisTrain = findViewById(R.id.enableTrainingButton);
         EnDisTrain.setEnabled(true);
         EnDisTrain.setTextColor(Color.parseColor("#FFFFFFFF"));
+
+        //disable the disable voice training button when page is first opened
         DisableTrainer = findViewById(R.id.stopVoiceRecording);
         DisableTrainer.setEnabled(false);
         DisableTrainer.setTextColor(Color.parseColor("#FF424242"));
 
         /*mp3/ogg/wav*/
-        outputF = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.mp3";
+        outputF = getExternalCacheDir().getAbsolutePath();
+        outputF += "/voice_trainer_recorder_audio.wav";
 
-        //ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
-        /*Commented out due to functionality not working
-        trainerAudio = new MediaRecorder();
-        trainerAudio.setAudioSource(MediaRecorder.AudioSource.MIC);
-        trainerAudio.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        trainerAudio.setOutputFile(outputF);
-        trainerAudio.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        */
         EnDisTrain.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -83,32 +97,30 @@ public class TrainingPage extends AppCompatActivity {
 
 
                 try {
-                    /*Commented out due to functionality not working
-                    trainerAudio.prepare();
-                    trainerAudio.start();
-                    */
-                    TextView textview= /*(TextView)*/findViewById(R.id.phonetic_Pangrams_Read_Text);
-                    TextView prText= /*(TextView)*/findViewById(R.id.tvPleaseRead);
+                    //Commented out due to functionality not working
+                    startRecording();
+
+
+                    //Display the phonetic pangrams text for the user to read
+                    TextView textview = findViewById(R.id.phonetic_Pangrams_Read_Text);
+                    TextView prText = findViewById(R.id.tvPleaseRead);
                     textview.setVisibility(View.VISIBLE);
                     prText.setVisibility(View.VISIBLE);
+
+                    //enable the disable voice training button for recording
                     DisableTrainer.setEnabled(true);
                     DisableTrainer.setTextColor(Color.parseColor("#FFFFFFFF"));
+
+                    //disable the enable voice training button for recording since it is opened
                     EnDisTrain.setEnabled(false);
                     EnDisTrain.setTextColor(Color.parseColor("#FF424242"));
+
                     Toast.makeText(getApplicationContext(), "Recording your voice...", Toast.LENGTH_SHORT).show();
-                } catch (IllegalStateException ie) {
+                }
+                catch (IllegalStateException ie) {
                     Toast.makeText(getApplicationContext(), "Illegal state", Toast.LENGTH_SHORT).show();
                     ie.printStackTrace();
-
                 }
-                /*Commented out due to functionality not working
-                catch (IOException ioe) {
-                    Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
-                    //Log.e(LOG_TAG, "prepare() failed");
-                    ioe.printStackTrace();
-                }*/
-                //trainerAudio.start();
-
             }
         });
 
@@ -117,20 +129,22 @@ public class TrainingPage extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                TextView textview=/*(TextView)*/ findViewById(R.id.phonetic_Pangrams_Read_Text);
-                TextView prText=/*(TextView)*/ findViewById(R.id.tvPleaseRead);
+                stopRecoring();
+
+                //hide the phonetic pangrams text
+                TextView textview = findViewById(R.id.phonetic_Pangrams_Read_Text);
+                TextView prText = findViewById(R.id.tvPleaseRead);
                 textview.setVisibility(View.INVISIBLE);
                 prText.setVisibility(View.INVISIBLE);
+
+                //since disable buton already clicked on, disable it
                 DisableTrainer.setEnabled(false);
                 DisableTrainer.setTextColor(Color.parseColor("#FF424242"));
+
+                //enable the enable voice training button
                 EnDisTrain.setEnabled(true);
                 EnDisTrain.setTextColor(Color.parseColor("#FFFFFFFF"));
-                //problem with recording audio
-                /*Commented out due to functionality not working
-                trainerAudio.stop();
-                trainerAudio.release();
-               */
-                trainerAudio = null;
+
                 Toast.makeText(getApplicationContext(), "Voice recording disabled", Toast.LENGTH_SHORT).show();
             }
         });
