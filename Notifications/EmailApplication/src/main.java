57920d155df1c859a5a6e2172285717cf24027f3
GreenLizzerd <31959787+GreenLizzerd@users.noreply.github.com>
2020-04-16 17:50:56 +0200

    Update main.java
    trying to test mail

--- Notifications/EmailApplication/src/main.java
+++ Notifications/EmailApplication/src/main.java
@@ -6,16 +6,16 @@ public class main {
     
     public static void main (String[]args)
     {
-//        JSONObject json = new JSONObject();
+        JSONObject json = new JSONObject();
         
-//        json.put("UserEmail","");
-//        json.put("UserName","squidBro");
-//        json.put("MsgType", "validate");
-//        json.put("Subject", "My 301 email");
-//        json.put("Message","Hello there, General Grievis");
+        json.put("UserEmail","cosomegatest@gmail.com");
+        json.put("UserName","squidBro");
+        json.put("MsgType", "validate");
+        json.put("Subject", "My 301 email");
+        json.put("Message","Hello there, General Grievis");
         
-//        
-//        Email.email(json);
+        
+        Email.email(json);
         
         
     }
