3aaa637dbf998832c91ac6c85fbc37d96611ff62
Ronald Looi <u15226532@tuks.co.za>
2020-04-16 10:39:53 +0200

    Image UPloading - choosing the 12 images

--- app/src/main/java/com/omega/mouthpiece/mouthCreation_ImageUpload.java
+++ app/src/main/java/com/omega/mouthpiece/mouthCreation_ImageUpload.java
@@ -23,12 +23,23 @@ public class mouthCreation_ImageUpload extends AppCompatActivity {
     private TextView mouthShapeNumber;
     private ImageView egImage;
     private ImageView userImage;
+    private ImageView userImage2;
+    private ImageView userImage3;
+    private ImageView userImage4;
+    private ImageView userImage5;
+    private ImageView userImage6;
+    private ImageView userImage7;
+    private ImageView userImage8;
+    private ImageView userImage9;
+    private ImageView userImage10;
+    private ImageView userImage11;
+    private ImageView userImage12;
     private int i = 1;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if( requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null)
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null)
         {
             Uri selectedImage = data.getData();
             userImage.setImageURI(selectedImage);
@@ -46,17 +57,41 @@ public class mouthCreation_ImageUpload extends AppCompatActivity {
         mouthShapeNumber = findViewById(R.id.textView2);
         egImage = findViewById(R.id.imageView2);
         userImage = findViewById(R.id.imageView5);
+        userImage2 = findViewById(R.id.imageView4);
+        userImage3 = findViewById(R.id.imageView6);
+        userImage4 = findViewById(R.id.imageView7);
+        userImage5 = findViewById(R.id.imageView8);
+        userImage6 = findViewById(R.id.imageView9);
+        userImage7 = findViewById(R.id.imageView10);
+        userImage8 = findViewById(R.id.imageView11);
+        userImage9 = findViewById(R.id.imageView12);
+        userImage10 = findViewById(R.id.imageView13);
+        userImage11= findViewById(R.id.imageView14);
+        userImage12 = findViewById(R.id.imageView15);
+
+        userImage.onVisibilityAggregated(true);
+        userImage.bringToFront();
+        userImage2.onVisibilityAggregated(false);
+        userImage3.onVisibilityAggregated(false);
+        userImage4.onVisibilityAggregated(false);
+        userImage5.onVisibilityAggregated(false);
+        userImage6.onVisibilityAggregated(false);
+        userImage7.onVisibilityAggregated(false);
+        userImage8.onVisibilityAggregated(false);
+        userImage9.onVisibilityAggregated(false);
+        userImage10.onVisibilityAggregated(false);
+        userImage11.onVisibilityAggregated(false);
+        userImage12.onVisibilityAggregated(false);
+
 
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivity(galleryIntent, savedInstanceState);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
 
             }
         });
-
-
 
         mouthShapeNumber.setText("Mouth Shape " + i + "/12");
         btnNext.setOnClickListener(new View.OnClickListener() {
@@ -72,36 +107,59 @@ public class mouthCreation_ImageUpload extends AppCompatActivity {
                     {
                         case 2:
                             egImage.setImageResource(R.drawable.mouth_formants_2);
+                            userImage.onVisibilityAggregated(false);
+                            userImage2.onVisibilityAggregated(true);
+                            userImage2.bringToFront();
                             break;
                         case 3:
                             egImage.setImageResource(R.drawable.mouth_formants_3);
+                            userImage2.onVisibilityAggregated(false);
+                            userImage3.onVisibilityAggregated(true);
                             break;
                         case 4:
                             egImage.setImageResource(R.drawable.mouth_formants_4);
+                            userImage3.onVisibilityAggregated(false);
+                            userImage4.onVisibilityAggregated(true);
                             break;
                         case 5:
                             egImage.setImageResource(R.drawable.mouth_formants_5);
+                            userImage4.onVisibilityAggregated(false);
+                            userImage5.onVisibilityAggregated(true);
                             break;
                         case 6:
                             egImage.setImageResource(R.drawable.mouth_formants_6);
+                            userImage5.onVisibilityAggregated(false);
+                            userImage6.onVisibilityAggregated(true);
                             break;
                         case 7:
                             egImage.setImageResource(R.drawable.mouth_formants_7);
+                            userImage6.onVisibilityAggregated(false);
+                            userImage7.onVisibilityAggregated(true);
                             break;
                         case 8:
                             egImage.setImageResource(R.drawable.mouth_formants_8);
+                            userImage7.onVisibilityAggregated(false);
+                            userImage8.onVisibilityAggregated(true);
                             break;
                         case 9:
                             egImage.setImageResource(R.drawable.mouth_formants_9);
+                            userImage8.onVisibilityAggregated(false);
+                            userImage9.onVisibilityAggregated(true);
                             break;
                         case 10:
                             egImage.setImageResource(R.drawable.mouth_formants_10);
+                            userImage9.onVisibilityAggregated(false);
+                            userImage10.onVisibilityAggregated(true);
                             break;
                         case 11:
                             egImage.setImageResource(R.drawable.mouth_formants_11);
+                            userImage10.onVisibilityAggregated(false);
+                            userImage11.onVisibilityAggregated(true);
                             break;
                         case 12:
                             egImage.setImageResource(R.drawable.mouth_formants_12);
+                            userImage11.onVisibilityAggregated(false);
+                            userImage12.onVisibilityAggregated(true);
                             break;
                     }
                 }
@@ -116,5 +174,6 @@ public class mouthCreation_ImageUpload extends AppCompatActivity {
             }
         });
     }
+
 
 }
