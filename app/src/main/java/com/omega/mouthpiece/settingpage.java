b4e047ee1268f48e65c51e1b2abb92d84a11ae37
Anika <anikavanrensburg@gmail.com>
2020-04-21 22:54:37 +0200

    Created settings page

---
+++ app/src/main/java/com/omega/mouthpiece/settingpage.java
@@ -0,0 +1,17 @@
+package com.omega.mouthpiece;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class settingpage extends AppCompatActivity {
+
+}
+
+
+
+
+
+
+
