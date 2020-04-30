4006a77df8c055c46dbd4c5f244ed488ac2a3250
Anika <anikavanrensburg@gmail.com>
2020-04-30 23:30:43 +0200

    Dark theme, somewhat working.

--- app/src/main/java/com/omega/mouthpiece/settingpage.java
+++ app/src/main/java/com/omega/mouthpiece/settingpage.java
@@ -9,19 +9,18 @@ public class settingpage extends AppCompatActivity {
 
 //    Switch simpleSwitch1;
 //
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.settings);
-//        // initiate view's
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
 //        simpleSwitch1 = (Switch) findViewById(R.id.switch2);
-//        simpleSwitch1.setOnCheckedChangeListener(new Switch().OnCheckedChangeListener() {
+//        simpleSwitch1.setOnCheckedChangeListener(new Switch().OnCheckedChangeListener()) {
 //            if (simpleSwitch1.isChecked()){
-//
+////
 //            }
 //            else{
 //
 //            }
-//        });
-//    }
+//        }
+    }
 }
