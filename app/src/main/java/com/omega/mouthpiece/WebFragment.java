d1d8bcf052f32c615e93c848942a8750a92ff91c
Kevin-D-H <u15026681@tuks.co.za>
2020-05-01 15:07:52 +0200

    Website minor update

---
+++ app/src/main/java/com/omega/mouthpiece/WebFragment.java
@@ -0,0 +1,23 @@
+package com.omega.mouthpiece;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
+public class WebFragment extends Fragment {
+
+    private WebView mywebview;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_website, container, false);
+        mywebview = root.findViewById(R.id.webView);
+        mywebview.loadUrl("http://www.mouthpiece.tech/");
+        return root;
+    }
+}
