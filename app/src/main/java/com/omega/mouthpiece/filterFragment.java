d622329d15e4b0abf63759f840ad5efacd9f5df7
Anrich96 <anrich96@gmail.com>
2020-05-02 14:21:47 +0200

    Added the filter page to the navigation and everything, but I don't know how to add the code to the filter button.

--- app/src/main/java/com/omega/mouthpiece/filterFragment.java
+++ app/src/main/java/com/omega/mouthpiece/filterFragment.java
@@ -12,11 +12,17 @@ import androidx.fragment.app.Fragment;
 
 public class filterFragment extends Fragment
 {
-    /*@Override
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        View fragmentView = inflater.inflate(R.layout.activity_mouth_selection2,container);
+        View fragmentView = inflater.inflate(R.layout.filter_page,container);
         return fragmentView;
-    }*/
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 }
 
