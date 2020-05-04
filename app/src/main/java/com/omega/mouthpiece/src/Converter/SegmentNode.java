ee592b531c7b3fe560d09611c29a535df5fb46c4
BritneyChu <56552783+BritneyChu@users.noreply.github.com>
2020-05-04 18:42:29 +0200

    Cloning omega-NN-V2 -(Incomplete: UserManTest() in test.java in construction)

---
+++ app/src/main/java/com/omega/mouthpiece/src/Converter/SegmentNode.java
@@ -0,0 +1,48 @@
+package Converter;
+
+public class SegmentNode
+{
+    public float[] audio = new float[120]; //break audio into smaller segments;
+    public int label;
+
+    public SegmentNode(float[] audio) //audio passed from top-down towards NN
+    {
+		//System.out.println("SegmentNode object created!");
+		label = 0;
+		for (int i = 0; i < audio.length; i++)
+		{
+			this.audio[i] = audio[i];
+		}
+    }
+
+    // I added this contructor so that I could use it inside the VoiceProfile class
+    public SegmentNode() {
+
+    }
+
+    public float[] getAudio()
+    {
+        return this.audio;
+    }
+    
+    public void printAudio(){
+       for(int i = 0; i < audio.length; i++) {
+            System.out.println("audio[" + i + "]" + " = " + this.audio[i] + " (with id = " + label + ")");
+        }		
+	}
+
+    public int getLabel()
+    {
+        return this.label;
+    }
+
+    public void setLabel(int label)
+    {
+        this.label = label;
+    }
+
+    public void setAudio(float[] audio){
+        this.audio = audio;
+    }
+
+}
---
+++ app/src/main/java/com/omega/mouthpiece/src/NN/Exceptions/SegmentExceptionNull.java
@@ -0,0 +1,9 @@
+package NN.Exceptions;
+
+public class SegmentExceptionNull extends Exception
+{
+    public SegmentExceptionNull(String message)
+    {
+        super(message);
+    }
+}
