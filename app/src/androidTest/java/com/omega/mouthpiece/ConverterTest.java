28a38205fdb53a4647897bab945598b6a5e7b963
ShaunVermeulen <u16078625@tuks.co.za>
2020-05-08 01:25:57 +0200

    Unit testing functionality now working again

--- app/src/androidTest/java/com/omega/mouthpiece/ConverterTest.java
+++ app/src/androidTest/java/com/omega/mouthpiece/ConverterTest.java
@@ -20,20 +20,20 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ConverterTest {
     @Rule
-    public ActivityTestRule displaysViewConv = new ActivityTestRule(Converter.class, true, false);
+    public ActivityTestRule displaysViewConv = new ActivityTestRule(ConverterUnit.class, true, false);
 
     @Test
     public void fomantButtonRead() throws Exception{
 
         displaysViewConv.launchActivity(new Intent());
-        onView(withText("Formant")).check(matches(isDisplayed()));
+        onView(withText(R.string.formant_based)).check(matches(isDisplayed()));
         onView(withId(R.id.btnFormant)).perform(click()).check(matches(isDisplayed()));
     }
     @Test
     public void volumeButtonRead() throws Exception{
 
         displaysViewConv.launchActivity(new Intent());
-        onView(withText("Volume")).check(matches(isDisplayed()));
+        onView(withText(R.string.volume_based)).check(matches(isDisplayed()));
         onView(withId(R.id.btn_record)).perform(click()).check(matches(isDisplayed()));
     }
 }
---
+++ app/src/main/java/com/omega/mouthpiece/ImageUploadUnit.java
@@ -0,0 +1,12 @@
+package com.omega.mouthpiece;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageUploadUnit extends AppCompatActivity {
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.fragment_mouthupload_front);
+    }
+}
