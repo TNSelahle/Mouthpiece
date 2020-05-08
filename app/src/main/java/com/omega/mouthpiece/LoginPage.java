02a7663eb2dca95f909e0a56f51b6ea3f9ce2a94
ShaunVermeulen <u16078625@tuks.co.za>
2020-05-08 03:36:32 +0200

    Skip button functionality working

--- app/src/main/java/com/omega/mouthpiece/LoginPage.java
+++ app/src/main/java/com/omega/mouthpiece/LoginPage.java
@@ -42,6 +42,7 @@ public class LoginPage extends AppCompatActivity {
     private CheckBox rememberMe;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private Button skipBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +58,7 @@ public class LoginPage extends AppCompatActivity {
         Login = findViewById(R.id.loginButton);
         Register = findViewById(R.id.registerButton);
         rememberMe = findViewById(R.id.rememberMe);
+        skipBtn = findViewById(R.id.skipBtn);
 
         sharedPreferences=getSharedPreferences("LoginPrefs",MODE_PRIVATE);
         editor=sharedPreferences.edit();
@@ -103,6 +105,16 @@ public class LoginPage extends AppCompatActivity {
             public void onClick(View view)
             {
                 Intent intent = new Intent(LoginPage.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+        skipBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(LoginPage.this, MainActivity.class);
                 startActivity(intent);
             }
         });
